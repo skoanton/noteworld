c65116e2e2fcdffab65252aa9d0a5d20345c6b79
skoanton <antonhakanssonn@gmail.com>
2025-01-28 20:54:46 +0100

    Add auto-update functionality using electron-updater and update version to 1.0.2

--- client/electron/electron-main.ts
+++ client/electron/electron-main.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
+import { autoUpdater } from 'electron-updater';
 let mainWindow: BrowserWindow | null;
 const isDev = !app.isPackaged;
 
@@ -25,10 +26,37 @@ app.on('ready', () => {
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
+
+    if (!isDev) {
+        autoUpdater.checkForUpdatesAndNotify();
+    }
+
 });
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
+});
+
+
+autoUpdater.on('update-available', () => {
+    console.log('En ny uppdatering finns tillgänglig!');
+});
+
+autoUpdater.on('update-not-available', () => {
+    console.log('Ingen uppdatering tillgänglig.');
+});
+
+autoUpdater.on('error', (err) => {
+    console.error('Fel vid uppdatering:', err);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+    console.log(`Nedladdning: ${progressObj.percent.toFixed(2)}%`);
+});
+
+autoUpdater.on('update-downloaded', () => {
+    console.log('Uppdatering nedladdad. Startar om...');
+    autoUpdater.quitAndInstall();
 });
